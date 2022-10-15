ec10dd4d61fc9be8d8245d89fdcdf0df1cabfbd7
SCool62 <79726681+SCool62@users.noreply.github.com>
2022-10-15 14:23:54 -0700

    Fix startup error

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/Arm.java
@@ -18,19 +18,24 @@ public class Arm implements SubSystem {
         armMotor = config.hardwareMap.get(DcMotor.class, Config.armMotor);
         // Reset the encoder and set it to be in RUN_TO_POSITION
         armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        //armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
     public void update() {
 
+//        if (config.gamePad2.a) {
+//            armMotor.setTargetPosition(1000);
+//        } else if (config.gamePad2.x) {
+//            armMotor.setTargetPosition(750);
+//        } else if (config.gamePad2.b) {
+//            armMotor.setTargetPosition(250);
+//        } else if(config.gamePad2.y) {
+//            armMotor.setTargetPosition(0);
+//        }
         if (config.gamePad2.a) {
-            armMotor.setTargetPosition(1000);
-        } else if (config.gamePad2.x) {
-            armMotor.setTargetPosition(750);
-        } else if (config.gamePad2.b) {
-            armMotor.setTargetPosition(250);
-        } else if(config.gamePad2.y) {
-            armMotor.setTargetPosition(0);
+            armMotor.setPower(-1);
+        } else if (config.gamePad2.y) {
+            armMotor.setPower(1);
         }
     }
 }
