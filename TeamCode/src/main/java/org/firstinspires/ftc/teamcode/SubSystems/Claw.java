79b82ddf444e2b097f38ad849e96171696cc8e23
SCool62 <79726681+SCool62@users.noreply.github.com>
2022-10-14 17:05:37 -0700

    Change claw to be on Gamepad2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/Claw.java
@@ -21,10 +21,10 @@ public class Claw implements SubSystem {
 
     @Override
     public void update() {
-        if (config.gamePad1.left_bumper) {
+        if (config.gamePad2.left_bumper) {
             // TODO: MAKE CLAW CLOSE
             clawServo.setPosition(1);
-        } else if (config.gamePad1.right_bumper) {
+        } else if (config.gamePad2.right_bumper) {
             // TODO: MAKE CLAW OPEN
             clawServo.setPosition(0);
         }
