4de38452667cdb312c989b3b159ac9c3db011d43
SCool62 <79726681+SCool62@users.noreply.github.com>
2023-08-30 15:58:41 -0700

    Create Auto superclass and add `moveForward`

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/Auto.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.autos;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.Config;
+
+public abstract class Auto extends LinearOpMode {
+    protected DcMotor leftFrontDrive;
+    protected DcMotor leftBackDrive;
+    protected DcMotor rightFrontDrive;
+    protected DcMotor rightBackDrive;
+
+    protected void initMotors() {
+        leftFrontDrive = hardwareMap.get(DcMotor.class, Config.LEFT_FRONT_DRIVE);
+        leftBackDrive = hardwareMap.get(DcMotor.class, Config.LEFT_BACK_DRIVE);
+        rightFrontDrive = hardwareMap.get(DcMotor.class, Config.RIGHT_FRONT_DRIVE);
+        rightBackDrive = hardwareMap.get(DcMotor.class, Config.RIGHT_BACK_DRIVE);
+    }
+
+
+    public void moveForward(long milliseconds) {
+        leftFrontDrive.setPower(1);
+        leftBackDrive.setPower(1);
+        rightFrontDrive.setPower(1);
+        rightBackDrive.setPower(1);
+        sleep(milliseconds);
+        leftFrontDrive.setPower(0);
+        leftBackDrive.setPower(0);
+        rightFrontDrive.setPower(0);
+        rightBackDrive.setPower(0);
+    }
+
+}
